627c81ad08f758177d5586c72aac2c3430dc643d
sena <kandasena@kandaseinanoMacBook-Pro.local>
2017-11-14 23:48:38 +0900

    ゲーム盤用の `ViewController` を用意

---
+++ Reversi/BoardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BoardViewController.swift
+//  Reversi
+//
+//  Created by Kanda Sena on 2017/11/14.
+//
+
+import Foundation
+import UIKit
+
+final class BoardViewController: UIViewController {
+    
+    /// ボードの行用スタック
+    @IBOutlet weak var verticalStack: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
